58493314f820157f9265b302bc75bb3cec32f532
jorgetvm <58443510+jorgetvm@users.noreply.github.com>
2020-05-10 14:23:00 +0200

    PRIMERA SUBIDA
    PRIMERA SUBIDA

---
+++ precache-manifest.4b5aeb686a0a2570217bfa3621c7dce9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb07f120dcb53adc9ebfdc38e196aeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "162476bdecc5da4db9ec",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "84a352f3b4e076913b50",
+    "url": "/static/css/main.70bc21d5.chunk.css"
+  },
+  {
+    "revision": "162476bdecc5da4db9ec",
+    "url": "/static/js/2.3b973bb0.chunk.js"
+  },
+  {
+    "revision": "5657dca68abcbbcb8243ceba173d3c7e",
+    "url": "/static/js/2.3b973bb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a352f3b4e076913b50",
+    "url": "/static/js/main.76846ee8.chunk.js"
+  },
+  {
+    "revision": "d0a3f0e72d91c64fdb9e",
+    "url": "/static/js/runtime-main.04cf1f90.js"
+  },
+  {
+    "revision": "3cdc8945d19c4a67e8be658088757869",
+    "url": "/static/media/pelota.3cdc8945.png"
+  }
+]);
