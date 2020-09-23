cb9481293fc10653ff5138b2be780d12839a01fe
jorgetvm <58443510+jorgetvm@users.noreply.github.com>
2020-09-23 23:14:38 +0200

    v2.01

---
+++ precache-manifest.00e194d1de3569fa5bff145ca48ab25f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1272525d7bb299ff21459335f6a876",
+    "url": "./index.html"
+  },
+  {
+    "revision": "692616a0d72029d2ae25",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "76de6875d1c759a98314",
+    "url": "./static/css/main.fed52ff2.chunk.css"
+  },
+  {
+    "revision": "692616a0d72029d2ae25",
+    "url": "./static/js/2.dc61efe9.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "./static/js/2.dc61efe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76de6875d1c759a98314",
+    "url": "./static/js/main.2d51e3d8.chunk.js"
+  },
+  {
+    "revision": "20d0b9e776646d08100b",
+    "url": "./static/js/runtime-main.4a5975a8.js"
+  },
+  {
+    "revision": "118fe928d54a31bb3a09d35093803172",
+    "url": "./static/media/GMatch_NEW_WHITE.118fe928.png"
+  },
+  {
+    "revision": "8d965b4cd7d6f62fd9aa1bc5c3c266f7",
+    "url": "./static/media/MCT_REAL_neg.8d965b4c.png"
+  },
+  {
+    "revision": "3cdc8945d19c4a67e8be658088757869",
+    "url": "./static/media/pelota.3cdc8945.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.de2beef4696549637bcb71714cb9b07d.js"
+  "./precache-manifest.00e194d1de3569fa5bff145ca48ab25f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
