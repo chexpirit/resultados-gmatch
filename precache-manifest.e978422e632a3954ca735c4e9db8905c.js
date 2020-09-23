8a8256b8d70201a09344dd879fb37e813d66206c
jorgetvm <58443510+jorgetvm@users.noreply.github.com>
2020-09-23 23:18:25 +0200

    v2.02

---
+++ precache-manifest.e978422e632a3954ca735c4e9db8905c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711fb125d78b41c693ad95e9dbc032a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "692616a0d72029d2ae25",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f4f129bc7ff6409ca8ef",
+    "url": "/static/css/main.df22c55c.chunk.css"
+  },
+  {
+    "revision": "692616a0d72029d2ae25",
+    "url": "/static/js/2.dc61efe9.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.dc61efe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4f129bc7ff6409ca8ef",
+    "url": "/static/js/main.61a28a8d.chunk.js"
+  },
+  {
+    "revision": "d0a3f0e72d91c64fdb9e",
+    "url": "/static/js/runtime-main.04cf1f90.js"
+  },
+  {
+    "revision": "118fe928d54a31bb3a09d35093803172",
+    "url": "/static/media/GMatch_NEW_WHITE.118fe928.png"
+  },
+  {
+    "revision": "8d965b4cd7d6f62fd9aa1bc5c3c266f7",
+    "url": "/static/media/MCT_REAL_neg.8d965b4c.png"
+  },
+  {
+    "revision": "3cdc8945d19c4a67e8be658088757869",
+    "url": "/static/media/pelota.3cdc8945.png"
+  }
+]);
