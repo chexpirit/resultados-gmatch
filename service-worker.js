066f0910d3575318a7291f964dd28f8c6c7ffeb9
jorgeMB <jorge.mullorbuitrago@telefonica.com>
2022-04-21 20:59:18 +0200

    new look led

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6f52eafdae5b5db6306af85fd6913448.js"
+  "./precache-manifest.72e68608ddbd17139d01edae6f7b14a6.js"
 );
 
 self.addEventListener('message', (event) => {
